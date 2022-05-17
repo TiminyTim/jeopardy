b33d5a918503d93e76ccea1e8152cefc82dca9fe
Timmy <timinytim@gmail.com>
2022-05-16 19:37:15 -0700

    Made the trivia game *more* functional.

--- Client.java
+++ Client.java
@@ -17,47 +17,67 @@ public class Client {
 				Socket mySocket = new Socket(hostName, portNumber);
 				PrintWriter socket_out = new PrintWriter(mySocket.getOutputStream(), true);
 				BufferedReader socket_in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
-			) {
+		    ) {
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 			String fromServer;
 			String fromUser;
 			String userName;
 			System.out.println("Please enter your name:");
-			// the thread will sleep for n milli seconds   
 			fromUser = stdIn.readLine();
 			userName = fromUser; //FIXME In case we need to save username?
 			socket_out.println(fromUser); //OUT - Transmit name over the interwebs
-			System.out.println(socket_in.readLine()); //IN - Accept Welcome
-			socket_out.println(); //OUT - tennis
-			//System.out.println(socket_in.readLine()); //IN - Accept start game
-			//try { Thread.sleep(2000); } catch (InterruptedException e) { System.out.println(e); }    
+
+			fromServer = socket_in.readLine();//IN - Welcome message
+			System.out.println(fromServer);
+			socket_out.println(""); //OUT - tennis?? TODO
+
+			fromServer = socket_in.readLine(); //IN - Accept start game?
+			System.out.println(fromServer);
+			socket_out.println(""); //EMPTY OUT?
+
+			//HIT ENTER to start game
+			//fromUser = stdIn.readLine();
+			//socket_out.println(stdIn.readLine());
+
 			//System.out.print("\033[H\033[2J");//WIPE SCREEN
 
-			//System.out.println(socket_in.readLine()); //IN 
-
-			socket_out.println(stdIn.readLine());
-			//socket_out.println("I don't think this will appear");
-			while ((fromServer = socket_in.readLine()) != null) { 
-				System.out.println(socket_in.readLine()); //IN 
-			System.out.println("I'm going bonker");
-				System.out.println("A: " + socket_in.readLine());
+			while (true) { 
+				//while ((fromServer = socket_in.readLine()) != null) { 
+				//Get Question
+				fromServer = socket_in.readLine(); //IN
+				System.out.println(fromServer);  
 				if (fromServer.equals("QUIT")) break;
 
-				fromUser = stdIn.readLine(); //IN - Read line from the keyboard
-				
-				if (fromUser != null) {
-					System.out.println("Client " + userName + ": " + fromUser); //OUT
-					socket_out.println(fromUser);
+				//Get answer
+				System.out.print("Answer: ");
+				while (true) {
+					fromUser = stdIn.readLine(); //LOCAL
+					if (fromUser != null) {
+						System.out.println("Client " + userName + ": " + fromUser); //OUT
+						socket_out.println(fromUser);
+						break;
+					}
 				}
+
+				//Get Right/Wrong?
+				fromServer = socket_in.readLine(); //IN
+				System.out.println(fromServer);  
+				//The thread will sleep for n milli seconds   
+				//try { Thread.sleep(2000); } catch (InterruptedException e) { System.out.println(e); }    
+				if (fromServer.equals("QUIT")) break;
+				socket_out.println(""); //EMPTY OUT
+				}
+			} 
+			catch (UnknownHostException e) {
+				System.err.println("Don't know about host " + hostName);
+				System.exit(1);
+			} 
+			catch (IOException e) {
+				System.err.println("Couldn't get I/O for the connection to " +
+						hostName);
+				System.exit(1);
+
 			}
-		} catch (UnknownHostException e) {
-			System.err.println("Don't know about host " + hostName);
-			System.exit(1);
-		} catch (IOException e) {
-			System.err.println("Couldn't get I/O for the connection to " +
-					hostName);
-			System.exit(1);
-		}
+		    }
 	}
-}
 
--- Server.java
+++ Server.java
@@ -1,37 +1,46 @@
-//You: Put your and your parter's names here
-//You: Describe which bullet points you implemented
-//You: If you did anything worthy of extra credit, put them here
+//Java Jeapordy Programming Assignment
+//Portnoff
+//Lara
+//Cromwell
+//Cruz
 
-//Point Yi) Have the server automatically begin the game when three (exactly three) clients
+//Point 1) Have the server automatically begin the game when three (exactly three) clients
 //connect. Before the game begins, the only thing that should happen is that the
 //clients should enter their name for the scoreboard.
+//	Game waits for *TWO* players to start the game.
+//	I only did this for speed.
 
-//Point Er) Once three people are in, then the game begins and should ask one
+//Point 2) Once three people are in, then the game begins and should ask one
 //question at a time to the clients. Everyone needs to be on the same question
 //at the same time. In other words, don't just move on to the next question when
 //the first client responds - ALL of them have to give a response to move on to
 //the next question. (Hint: Use a ConcurrentHashMap to track client responses.)
-//hack 
+//	Game waits for 3 players on:
+//	Game start
+//	Each round of questions
 
-//Point San) After each question, send to the clients the current score for
+//Point 3) After each question, send to the clients the current score for
 //every player, so people can see how they're doing. They should win points for
 //getting questions right and lose points for getting questions wrong. As the
 //quiz progresses, the point value of the questions should go up.
+//	Points are incremented like this: 1, 2, 4, 8, 16, 32, 64...
+//	If the user gets it wrong, the user's points are / 2
 
-//Point Yon) You need to have some sort of data structure holding all of the
+//Point 4) You need to have some sort of data structure holding all of the
 //quiz questions and responses. Don't just cheap out with a bunch of if
 //statements or something. The topic for the quiz can be anything you like, and
 //the answers can either be multiple choice or free response or whatever.
-//TODO Change the way our questions and answers are streamed into our data structure
-//So far, we are reading in from a file that contains the questions, answers, and answer key
-//They can have any number of questions, T/F, multiple choice etc
-//Located in "questions.txt" and delimited by "_"
+//	ConcurrentHashMap for questions
+//	ConcurrentHashMap for answers
+//	Topics: https://docs.google.com/document/d/1TzrDXkeqjz-WukF6nbZQvypQaGjQbxHKHHWybWbPi8s/edit?usp=sharing
 
-//Point Cinco) When the game is over, it needs to tell the winner (the person(s)
+//Point 5) When the game is over, it needs to tell the winner (the person(s)
 //with the high score) they won, and to tell everyone else they're a loser. The
 //server should then reset itself back to a starting state so another three
 //people can connect. Some of the code right now will absolutely not work a
 //second time through unless you fix this up.
+//	TODO
+
 import java.net.*;
 import java.io.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -48,8 +57,7 @@ public class Server {
 	//This variable is only used by main() so we just make a normal Integer
 	static Integer thread_count = 0; //How many clients are connected
 	static AtomicInteger connected_threads = new AtomicInteger(); 
-	public static AtomicInteger chat_count = new AtomicInteger(); 
-	static AtomicInteger r = new AtomicInteger(); //Keep track of our responses per round 
+	static AtomicInteger chat_count = new AtomicInteger(); 
 
 	//Note: A ConcurrentHashMap is a thread-safe hash table you can share between threads
 	// You can use .get() to get data from it and .put() to put data into it
@@ -80,33 +88,34 @@ public class Server {
 			try (
 					PrintWriter socket_out = new PrintWriter(socket.getOutputStream(), true);
 					BufferedReader socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					//DataInputStream socket_in = new DataInputStream(new DataInputStream(socket.getInputStream()));
 			    ) {
 				String inputLine;
 				String outputLine;
+				//inputLine = socket_in.readLine(); //IN - Get their name from the network connection
+				//inputLine = socket_in.readLine(); //IN - Get their name from the network connection
 				inputLine = socket_in.readLine(); //IN - Get their name from the network connection
 				outputLine = "Welcome " + inputLine;
 				socket_out.println(outputLine); //OUT - Write a welcome message to the network connection
-				socket_in.readLine();
+				inputLine = socket_in.readLine(); //IN
 
-				//After they sign in with a username, increment players that are ready to start the game
-				connected_threads.incrementAndGet();
-
-				//Save their name into the ConcurrentHashMap
-				names.put(thread_id,inputLine);
-
-				//Set our score to 0 in the ConcurrentHashMap to begin with
-				scoreboard.put(thread_id, 0);
+				//connected_threads.incrementAndGet(); //After they sign in with a username, increment players that are ready to start the game
+				connected_threads.getAndIncrement(); //After they sign in with a username, increment players that are ready to start the game
+				names.put(thread_id,inputLine); //Save their name into the ConcurrentHashMap
+				scoreboard.put(thread_id, 0); //Set our score to 0 in the ConcurrentHashMap to begin with
 
 				//Stop the game until the minimum number of players have connected.
 				while (true) {
 					if (connected_threads.get() == 2) {
-						//socket_out.println("All players have connected! Welcome to Java Jeopardy! Press ENTER"); 
+						socket_out.println("All players have connected! Welcome to Java Jeopardy!"); //OUT
+						inputLine = socket_in.readLine(); //IN
 						break;
 					}
 				}
+				System.out.println("While done");
 
-				int f = 0;
 				//socket_in.readLine();
+				int f = 0;
 				//while ((f < 10) && ((inputLine = socket_in.readLine()) != null)) 
 				while (f < q.size()) {
 					System.out.println("Thread " + thread_id + " read: " + inputLine);
@@ -117,93 +126,104 @@ public class Server {
 
 					String question = q.get(f);
 					String answer   = a.get(f);
-					f = f + 1; // MOVE TODO
+					f = f + 1; // MOVE TODO????
 
-					socket_out.println(q.get(f)); //OUT - Send Question
-					inputLine = socket_in.readLine(); //IN - Receive answer
-					System.out.println("Answer: " + inputLine);
-					System.out.println("Actual: " + answer);
-
-					if (inputLine == answer) System.out.println("Answers match!!!!");
-					chat_count.incrementAndGet();
+					//Show server our answer
+					System.out.println("Actual: " + answer); //Show answer
+					
+					//Send, and receive question and answer respectively.
+					socket_out.println(question); //OUT
+					inputLine = socket_in.readLine(); //IN
 
 					//Check for all client responses
+					//chat_count.incrementAndGet();
+					chat_count.set(chat_count.get() + 1);
 					while (true) {
-						if (chat_count.get() == 2) { 
-							System.out.println("YEEHAW, Chat is equal to 2 and will now be reset to 0"); 
+						if (chat_count.get() % 2 == 0) { 
 							chat_count.set(0);
 							break;
 						}
 					}
-
-					if (Math.random() < 0.5) { //50/50 chance of getting a question right
-						//score += 100;
-						socket_out.println("Correct! Your score is now: " + score);
+					if (inputLine.equals(answer)) {
+						if (score == 0) score = 1;
+						else score *= 2;
+						socket_out.println("Right! The correct answer was: " + answer + ". Your score: " + score); //OUT
 					}
 					else { //Wrong answer!
-						//score -= 100;
-						socket_out.println("Wrong! Your score is now: " + score);
+						score /= 2;
+						socket_out.println("Wrong! The correct answer was: " + answer + ". Your score: " + score); //OUT
 					}
 
+					//Update scoreboard for each client
 					scoreboard.put(thread_id,score);
 
 					//Note: This prints the current scoreboard, delete it if it gets too spammy
 					System.out.println("====== Scoreboard ======");
 					//Note that this assumes we only have one game, it won't work with a second game, etc.
 					//But it should show you the way, so I am leaving it here
-					//for (int i = 0; i < thread_count; i++) 
-					for (int i = 0; i < connected_threads.get(); i++) {
+					for (int i = 0; i < thread_count; i++) {
+						//for (int i = 0; i < connected_threads.get(); i++) 
 						System.out.println(names.get(i) + ": " + scoreboard.get(i));
+						}
+
 					}
+					System.out.println("Thread closing");
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			    }
+		}
+
+
+		public static void main(String[] args) throws IOException {
+			//Usage vetting
+			if (args.length != 1) {
+				System.err.println("Usage: java Server <port number>");
+				System.exit(1);
+			}
+			int portNumber = Integer.parseInt(args[0]);
+
+			{ // Try to load our questions and answers
+				try {
+					File myObj = new File("peggy.txt"); //Name Peggy because she is the trivia master
+					Scanner cin = new Scanner(myObj); //I want to go back to C++
+					int i = 0;
+					while (cin.hasNextLine()) {
+						String question = cin.nextLine();
+						q.put(i, question);
+						String answer = cin.nextLine();
+						a.put(i, answer);
+						i = i + 1;	
+					}
+					cin.close();
+				} 
+				catch (FileNotFoundException e) {
+					System.out.println("An error occurred.");
+					e.printStackTrace();
 
 				}
-				System.out.println("Thread closing");
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			}
+			System.out.println(q.get(0));
+			System.out.println(q.get(1));
+			System.out.println(q.get(2));
+			System.out.println(q.get(3));
+			System.out.println(q.get(4));
+			System.out.println(q.get(5));
+
+			{ //Spawn thread
+				try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+					while (true) {
+						ServerThread new_thread = new ServerThread(serverSocket.accept(),thread_count); 
+						new_thread.start();
+						System.out.println("Client " + Integer.toString(thread_count) + " connected");
+						thread_count++;
+					}
+				} 
+				catch (IOException e) {
+					System.err.println("Could not listen on port " + portNumber);
+					System.exit(-1);
+				}
 			}
 		}
 	}
-
-
-	public static void main(String[] args) throws IOException {
-		//Usage vetting
-		if (args.length != 1) {
-			System.err.println("Usage: java Server <port number>");
-			System.exit(1);
-		}
-
-		try { // Try to load our questions and answers
-			File myObj = new File("peggy.txt"); //Name Peggy because she is the trivia master
-			Scanner cin = new Scanner(myObj); //I want to go back to C++
-			int i = 0;
-			while (cin.hasNextLine()) {
-				String question = cin.nextLine();
-				q.put(i, question);
-				String answer = cin.nextLine();
-				a.put(i, answer);
-				i = i + 1;	
-			}
-			cin.close();
-		} 
-		catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-
-		int portNumber = Integer.parseInt(args[0]);
-
-		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-			while (true) {
-				ServerThread new_thread = new ServerThread(serverSocket.accept(),thread_count); 
-				new_thread.start();
-				System.out.println("Client " + Integer.toString(thread_count) + " connected");
-				thread_count++;
-			}
-		} 
-		catch (IOException e) {
-			System.err.println("Could not listen on port " + portNumber);
-			System.exit(-1);
-		}
-	}
-}
